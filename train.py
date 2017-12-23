ecf315f6ebef8750cab5ea686c2faa87914bd9d0
Felix Parker <flixpar@gmail.com>
2017-12-23 18:39:47 -0500

    improved trainer

--- train.py
+++ train.py
@@ -2,6 +2,11 @@ import torch
 from torch import nn
 from torch import optim
 
+from dataloader import VisDaDataLoader
+from gcn import GCN
+
+import os
+
 # config:
 epochs = 100
 batch_size = 16
@@ -9,13 +14,16 @@ lr = 1e-4
 weight_decay = 2e-5
 momentum = 0.9
 
-optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+base_path = ""
+save_path = os.path.join(base_path, "saves", "gcn-%d.pth")
 
-criterion = nn.CrossEntropyLoss(weight.cuda())
+data = VisDaDataLoader()
+model = GCN(data.num_classes, data.img_size)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+criterion = nn.CrossEntropyLoss(data.class_weights.cuda())
 
 for epoch in range(epochs):
-
-	loss =
 
 	loss.backward()
 	optimizer.step()
