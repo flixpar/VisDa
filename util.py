9e4fdbedfd9faa641eae6fd3bc9ac9c66fdb5950
Felix Parker <flixpar@gmail.com>
2017-12-24 14:50:57 -0500

    working on bug in loss function

--- util.py
+++ util.py
@@ -19,11 +19,12 @@ class CrossEntropyLoss2d(nn.Module):
 class CrossEntropyLoss2dMax(nn.Module):
     def __init__(self, weight=None, size_average=True, ignore_index=None):
         super(CrossEntropyLoss2dMax, self).__init__()
-        self.softmax = nn.LogSoftmax(dim=1)
+        #self.softmax = nn.LogSoftmax(dim=1)
         self.nll_loss = nn.NLLLoss2d(weight, size_average, ignore_index)
 
     def forward(self, inputs, targets):
-        return self.nll_loss(self.softmax(inputs).max(dim=1), targets)
+        #return self.nll_loss(self.softmax(inputs), targets)
+		return self.nll_loss(F.log_softmax(inputs).max(dim=1), targets)
 
 def cross_entropy2d(input, target, weight=None, size_average=True):
         n, c, h, w = input.size()
