d97d668f4c7c6e384177b990ad83767dc6c74f2c
flixpar <flixpar@gmail.com>
2018-01-03 17:58:39 +0000

    worked on class_freq script

--- scripts/class_freq.py
+++ scripts/class_freq.py
@@ -9,9 +9,10 @@ import numpy as np
 
 import random
 
-base_path = "/home/flixpar/data/VisDa/train/annotations"
-sample_freq = 0.3
-processes = 5
+base_path = "/home/flixpar/data/train/annotations"
+sample_freq = 0.2
+processes = 2
+total_samples = 3
 
 labels = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (20, 20, 20), (111, 74, 0), (81, 0, 81), (128, 64, 128),
 	          (244, 35, 232), (250, 170, 160), (230, 150, 140), (70, 70, 70), (102, 102, 156), (190, 153, 153),
@@ -21,21 +22,29 @@ labels = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (20, 20, 20), (111, 74, 0)
 
 
 image_paths = glob(os.path.join(base_path, "*.png"))
-img_ids = random.sample(len(image_paths), len(image_paths)*sample_freq)
-image_paths = [pth if i in img_ids for (i, pth) in enumerate(image_paths)]
+num_imgs = len(image_paths)
+num_samples = int(num_imgs * sample_freq)
+image_paths = random.sample(image_paths, total_samples)
 
-pool = mp.Pool(processes)
-data = pool.map(calc_stats, image_paths)
+def main():
 
-all_counts = np.array()
-for counts in data:
-	np.append(all_counts, counts)
-counts = np.sum(all_counts, axis=1)
+	pool = mp.Pool(processes)
+	data = pool.map(calc_stats, image_paths)
 
-print("counts: {}".format(counts))
+	all_counts = np.array()
+	for counts in data:
+		np.append(all_counts, counts)
+	counts = np.sum(all_counts, axis=1)
+
+	print("counts: {}".format(counts))
 
 def calc_stats(img_path):
-	img = cv2.imread(img_path,0)
-	unique, counts = np.unique(img, return_counts=True, axis=2)
-	full_counts = [counts[i] if i in unique else 0 for i in range(len(labels))]
+	img = cv2.imread(img_path)
+	print(img.shape)
+	unique, counts = np.unique(img, return_counts=True)
+	full_counts = [counts[i] if i in unique else 0 for i in range(35)]
 	return full_counts
+
+if __name__ == "__main__":
+    main()
+
