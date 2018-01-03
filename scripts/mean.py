adfbfa37d61735f234d334502db5da840d5f20e7
flixpar <flixpar@gmail.com>
2018-01-03 17:55:06 +0000

    found mean and stdev

--- scripts/mean.py
+++ scripts/mean.py
@@ -9,37 +9,45 @@ import numpy as np
 
 import random
 
-base_path = "/home/flixpar/data/VisDa/train/images"
-sample_freq = 0.3
-processes = 5
+base_path = "/home/flixpar/data/train/images"
+sample_freq = 0.2
+processes = 4
 
 image_paths = glob(os.path.join(base_path, "*.png"))
-img_ids = random.sample(len(image_paths), len(image_paths)*sample_freq)
-image_paths = [pth if i in img_ids for (i, pth) in enumerate(image_paths)]
+num_imgs = len(image_paths)
+num_samples = int(num_imgs * sample_freq)
+image_paths = random.sample(image_paths, num_samples)
 
-pool = mp.Pool(processes)
-data = pool.map(calc_stats, image_paths)
+print("Sampling {} images...".format(num_samples))
 
-avgs, stdevs = zip(*data)
-avgs_0, avgs_1, avgs_2 = zip(*avgs)
-stds_0, stds_1, stds_2 = zip(*stdevs)
+def main():
+    pool = mp.Pool(processes)
+    data = pool.map(calc_stats, image_paths)
 
-avg_0 = sum(avgs_0)/len(avgs_0)
-avg_1 = sum(avgs_1)/len(avgs_1)
-avg_2 = sum(avgs_2)/len(avgs_2)
+    avgs, stdevs = zip(*data)
+    avgs_0, avgs_1, avgs_2 = zip(*avgs)
+    stds_0, stds_1, stds_2 = zip(*stdevs)
 
-stds_0 = sum(stds_0)/len(stds_0)
-stds_1 = sum(stds_1)/len(stds_1)
-stds_2 = sum(stds_2)/len(stds_2)
+    avg_0 = sum(avgs_0)/len(avgs_0)
+    avg_1 = sum(avgs_1)/len(avgs_1)
+    avg_2 = sum(avgs_2)/len(avgs_2)
 
-mean = (avg_0, avg_1, avg_2)
-stdev = (stds_0, stds_1, stds_2)
+    stds_0 = sum(stds_0)/len(stds_0)
+    stds_1 = sum(stds_1)/len(stds_1)
+    stds_2 = sum(stds_2)/len(stds_2)
 
-print("Means: {}".format(mean))
-print("Standard deviations: {}".format(stdev))
+    mean = (avg_0, avg_1, avg_2)
+    stdev = (stds_0, stds_1, stds_2)
+
+    print("Means: {}".format(mean))
+    print("Standard deviations: {}".format(stdev))
 
 def calc_stats(img_path):
-	img = cv2.imread(img_path)
-	mean = np.mean(img, axis=2)
-	stdev = np.std(img, axis=2)
-	return (mean, stdev)
+    img = cv2.imread(img_path)
+    mean = np.mean(img, axis=(0,1))
+    stdev = np.std(img, axis=(0,1))
+    return (mean, stdev)
+
+if __name__ == "__main__":
+    main()
+
