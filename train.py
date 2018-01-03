77c9f095776782a47ce92ef66c7a81396c5d7875
flixpar <flixpar@gmail.com>
2018-01-03 17:57:04 +0000

    improvements to training

--- train.py
+++ train.py
@@ -9,14 +9,14 @@ from gcn import GCN
 from util import CrossEntropyLoss2d
 
 import os
-import tqdm
+from tqdm import tqdm
 
 # config:
 epochs = 25  # 100
-batch_size = 1
-lr = 1e-4
-weight_decay = 2e-5
-momentum = 0.9
+batch_size = 4
+lr = 2e-6
+weight_decay = 1e-6
+momentum = 0.95
 
 base_path = "/home/flixpar/VisDa"
 save_path = os.path.join(base_path, "saves", "gcn-%d.pth")
@@ -25,23 +25,21 @@ torch.backends.cudnn.benchmark = True
 # print("GPUs: {}".format(torch.cuda.device_count()))
 
 dataset = VisDaDataset()
-dataloader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1, pin_memory=True)
+dataloader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=6)
 
 model = GCN(dataset.num_classes, dataset.img_size).cuda()
 model.train()
 
-optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay, nesterov=True)
 criterion = CrossEntropyLoss2d().cuda()
 
 print("Starting training...")
 
 for epoch in range(epochs):
 
-	for (image, label) in tqdm.tqdm(dataloader, total=len(dataset)):
+	for i, (image, label) in tqdm(enumerate(dataloader), total=int(len(dataset)/batch_size)):
 		img = autograd.Variable(image.cuda())
 		lbl = autograd.Variable(label.cuda())
-		#img = autograd.Variable(image)
-		#lbl = autograd.Variable(label)
 
 		output = model(img)
 		loss = criterion(output, lbl)
@@ -49,6 +47,9 @@ for epoch in range(epochs):
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
+
+		if i % 100 == 0:
+			tqdm.write("loss: {}".format(loss.data[0]))
 
 	print("Epoch {} completed.".format(epoch + 1))
 
