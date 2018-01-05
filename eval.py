a25ffff01c3c219121b45048ad71d46dfe32a7b1
flixpar <flixpar@gmail.com>
2018-01-05 17:40:25 +0000

    minor fixes to eval

--- eval.py
+++ eval.py
@@ -9,18 +9,24 @@ import numpy as np
 from dataloader import VisDaDataset
 from metrics import scores
 
-save_path = "/home/flixpar/VisDa/saves/gcn-12.pth"
-samples = 5
+save_path = "/home/flixpar/VisDa/saves/gcn-8.pth"
+samples = 50
 out_path = "/home/flixpar/VisDa/pred/"
 
-dataset = VisDaDataset()
-dataloader = data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=1, pin_memory=True)
+K = 7
+scale_factor = 0.8
+img_size = (int(scale_factor * 1052), int(scale_factor * 1914))
 
-model = GCN(dataset.num_classes, dataset.img_size).cuda()
+dataset = VisDaDataset(im_size=img_size)
+dataloader = data.DataLoader(dataset, batch_size=1, shuffle=True)
+
+model = GCN(dataset.num_classes, dataset.img_size, k=K).cuda()
 model.load_state_dict(torch.load(save_path))
 model.eval()
 
 def main():
+
+	iou = 0
 
 	print("Starting predictions...")
 	for i, (image, truth) in enumerate(dataloader):
@@ -35,7 +41,11 @@ def main():
 		gt = np.squeeze(Variable(truth).data.cpu().numpy())
 
 		score, class_iou = scores(gt, pred, dataset.num_classes)
-		print_scores(i+1, score, class_iou)
+		iou += score['Mean IoU : \t']
+		# print_scores(i+1, score, class_iou)
+
+	iou /= samples
+	print("Mean IOU: {}".format(iou))
 
 
 def print_scores(i, score, class_iou):
