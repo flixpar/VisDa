4928e32d19050b2d89f68192d13025812c4f1469
Felix Parker <flixpar@gmail.com>
2018-01-06 03:05:19 -0500

    refactored train, added eval and lr_scheduler

--- train.py
+++ train.py
@@ -5,9 +5,10 @@ from torch import autograd
 from torch.utils import data
 torch.backends.cudnn.benchmark = True
 
-from dataloader import VisDaDataset
-from gcn import GCN
-from util import CrossEntropyLoss2d
+from models.gcn import GCN
+from loaders.dataloader import VisDaDataset
+from util.loss import CrossEntropyLoss2d
+from util.util import Namespace, poly_lr_scheduler
 
 import os
 from tqdm import tqdm
@@ -16,47 +17,81 @@ import yaml
 # config:
 config_path = "/home/flixpar/VisDa/config.yaml"
 args = Namespace(**yaml.load(open(config_path, 'r')))
-args.img_size = tuple(args.img_size)
 
+# logging:
 save_path = os.path.join(args.base_path, "saves", "gcn-{}.pth")
+logfile = open(os.path.join(args.base_path, "saves", "train.log"), 'w')
 
-log_file_path = os.path.join(args.base_path, "saves", "train.log")
-log_file = open(log_file_path, 'w')
-
+# data loading:
 dataset = VisDaDataset(im_size=args.img_size)
 dataloader = data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=6)
+evalloader = data.DataLoader(VisDaDataset(im_size=args.img_size, mode="eval"), batch_size=1, shuffle=True)
 
+# setup model:
 model = GCN(dataset.num_classes, dataset.img_size, k=args.K).cuda()
 model.train()
 
+# setup loss and optimizer
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
 criterion = CrossEntropyLoss2d(weight=dataset.class_weights).cuda()
 
-print("Starting training...")
-for epoch in range(args.max_epochs):
+def main():
 
-	for i, (image, label) in tqdm(enumerate(dataloader), total=int(len(dataset)/args.batch_size)):
-		img = autograd.Variable(image.cuda())
-		lbl = autograd.Variable(label.cuda())
+	print("Starting training...")
+	for epoch in range(args.max_epochs):
 
+		for i, (image, label) in tqdm(enumerate(dataloader), total=int(len(dataset)/args.batch_size)):
+			img = autograd.Variable(image.cuda())
+			lbl = autograd.Variable(label.cuda())
+
+			output = model(img)
+			loss = criterion(output, lbl)
+
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+
+			if i % 100 == 0:
+				tqdm.write("loss: {}".format(loss.data[0]))
+				logfile.write(str(loss.data[0])+"\n")
+
+			if i % args.eval_freq == 0:
+				miou = evaluate()
+				tqdm.write("Eval mIOU: {}".format(miou))
+				logfile.write("Eval mIOU: {}\n".format(miou))
+
+		print("Epoch {} completed.".format(epoch + 1))
+		logfile.write("Epoch {} completed.\n".format(epoch + 1))
+		torch.save(model.state_dict(), save_path.format(epoch + 1))
+
+		miou = evaluate()
+		tqdm.write("Eval mIOU: {}".format(miou))
+		logfile.write("Eval mIOU: {}\n".format(miou))
+
+		optimizer = poly_lr_scheduler(optimizer, args.lr, epoch,
+			lr_decay_iter=args.lr_decay_freq, max_iter=args.max_epochs, power=args.lr_power)
+
+	torch.save(model.state_dict(), save_path.format("final"))
+
+def evaluate():
+	model.eval()
+
+	iou = 0
+	samples = 0
+
+	for (img, lbl) in evalloader:
+		img = Variable(img.cuda())
 		output = model(img)
-		loss = criterion(output, lbl)
 
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+		pred = np.squeeze(output.data.max(1)[1].cpu().numpy())
+		gt = np.squeeze(lbl.cpu().numpy())
 
-		if i % 100 == 0:
-			tqdm.write("loss: {}".format(loss.data[0]))
-			log_file.write(str(loss.data[0])+"\n")
+		iou += miou(gt, pred, dataset.num_classes)
+		samples += 1
 
-	print("Epoch {} completed.".format(epoch + 1))
-	log_file.write("Epoch {} completed.\n".format(epoch + 1))
-	torch.save(model.state_dict(), save_path.format(epoch + 1))
+	model.train()
+	miou /= samples
+	return miou
 
-	if (epoch + 1) % args.lr_decay_freq == 0:
-		args.lr /= args.lr_decay_rate
-		optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=True)
-
-
-torch.save(model.state_dict(), save_path.format("final"))
+if __name__ == "__init__":
+	main()
