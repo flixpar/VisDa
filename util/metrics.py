c0da2dae69e454b585f33e2ddd97ebea043a2a3f
Felix Parker <flixpar@gmail.com>
2018-01-09 13:51:24 -0500

    added confusion matrix to metrics

--- util/metrics.py
+++ util/metrics.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import itertools
+from sklearn import metrics as skmetrics
 
 def _fast_hist(label_true, label_pred, n_class):
 	mask = (label_true >= 0) & (label_true < n_class)
@@ -52,3 +55,33 @@ def print_scores(pred, gt, n_classes, i=0):
 		if not np.isnan(val):
 			print("{}:\t{}".format(key, val))
 	print()
+
+def plotConfusionMatrix(gt, pred, num_classes, fn="confusion_matrix.png"):
+	cfm = skmetrics.confusion_matrix(gt.flatten(), pred.flatten())
+
+	plt.figure()
+
+	# normalize and display
+	cfm = cfm.astype('float') / cfm.sum(axis=1)[:, np.newaxis]
+	plt.imshow(cfm, interpolation='nearest', cmap=plt.cm.Blues)
+	
+	# setup the title and axes
+	plt.title("Normalized Confusion Matrix")
+	plt.colorbar()
+	tick_marks = np.arange(num_classes)
+	plt.xticks(tick_marks, range(num_classes), rotation=45)
+	plt.yticks(tick_marks, range(num_classes))
+
+	# add values
+	thresh = cfm.max() / 2.
+	for i, j in itertools.product(range(cfm.shape[0]), range(cfm.shape[1])):
+		plt.text(j, i, cfm[i, j], size="x-small", horizontalalignment="center", color = ("white" if cfm[i, j] > thresh else "black"))
+
+	# label the axes
+	plt.tight_layout()
+	plt.ylabel('True label')
+	plt.xlabel('Predicted label')
+	
+	# save image and display
+	plt.savefig(fn, dpi=100)
+	plt.show()
