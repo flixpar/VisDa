2a1a38587242c878d1dd60f40043a93be2930356
--global <--global>
2018-01-25 17:58:22 +0000

    improved stat calculator scripts

--- scripts/mean.py
+++ scripts/mean.py
@@ -9,35 +9,24 @@ import numpy as np
 
 import random
 
-base_path = "/home/flixpar/data/train/images"
-sample_freq = 0.2
-processes = 4
+base_path = "/media/data/train/images"
+sample_freq = 0.1
+processes = 6
 
 image_paths = glob(os.path.join(base_path, "*.png"))
 num_imgs = len(image_paths)
 num_samples = int(num_imgs * sample_freq)
 image_paths = random.sample(image_paths, num_samples)
 
-print("Sampling {} images...".format(num_samples))
+print("Sampling {} of {} images...".format(num_samples, num_imgs))
 
 def main():
     pool = mp.Pool(processes)
     data = pool.map(calc_stats, image_paths)
 
-    avgs, stdevs = zip(*data)
-    avgs_0, avgs_1, avgs_2 = zip(*avgs)
-    stds_0, stds_1, stds_2 = zip(*stdevs)
-
-    avg_0 = sum(avgs_0)/len(avgs_0)
-    avg_1 = sum(avgs_1)/len(avgs_1)
-    avg_2 = sum(avgs_2)/len(avgs_2)
-
-    stds_0 = sum(stds_0)/len(stds_0)
-    stds_1 = sum(stds_1)/len(stds_1)
-    stds_2 = sum(stds_2)/len(stds_2)
-
-    mean = (avg_0, avg_1, avg_2)
-    stdev = (stds_0, stds_1, stds_2)
+    means, stdevs = zip(*data)
+    mean = np.mean(means, axis=0)
+    stdev = np.mean(stdevs, axis=0)
 
     print("Means: {}".format(mean))
     print("Standard deviations: {}".format(stdev))
@@ -46,7 +35,7 @@ def calc_stats(img_path):
     img = cv2.imread(img_path)
     mean = np.mean(img, axis=(0,1))
     stdev = np.std(img, axis=(0,1))
-    return (mean, stdev)
+    return mean, stdev
 
 if __name__ == "__main__":
     main()
--- scripts/mean_cityscapes.py
+++ scripts/mean_cityscapes.py
@@ -8,25 +8,29 @@ import cv2
 import numpy as np
 from skimage.exposure import equalize_adapthist, rescale_intensity
 
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
 import random
 
 base_path = "/media/data/cityscapes/"
 sample_freq = 0.3
 processes = 6
 
-image_paths = glob.glob(os.path.join(base_path, "images", "val", "**", "*.png"), recursive=True)
+image_paths = glob(os.path.join(base_path, "images", "val", "**", "*.png"), recursive=True)
 num_imgs = len(image_paths)
 num_samples = int(num_imgs * sample_freq)
 image_paths = random.sample(image_paths, num_samples)
 
-print("Sampling {} images...".format(num_samples))
+print("Sampling {} images of {} total".format(num_samples, num_imgs))
 
 def main():
 	pool = mp.Pool(processes)
 	data = pool.map(calc_stats, image_paths)
 
-	mean = np.mean(data)
-	stdev = np.std(data)
+	means, stdevs = zip(*data)
+	mean = np.mean(means, axis=0)
+	stdev = np.mean(stdevs, axis=0)
 
 	print("Means: {}".format(mean))
 	print("Standard deviations: {}".format(stdev))
@@ -35,7 +39,8 @@ def calc_stats(img_path):
 	img = cv2.imread(img_path)
 	img = enhance_contrast(img)
 	mean = np.mean(img, axis=(0,1))
-	return mean
+	stdev = np.std(img, axis=(0,1))
+	return mean, stdev
 
 def enhance_contrast(img):
 	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
