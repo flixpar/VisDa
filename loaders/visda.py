1090f1ff7e2791ee5b88bc46c110459f036b38ff
Felix Parker <flixpar@gmail.com>
2018-01-29 22:37:47 -0500

    minor fixes

--- loaders/visda.py
+++ loaders/visda.py
@@ -76,66 +76,6 @@ class VisDaDataset(data.Dataset):
 		return (img, lbl)
 
 
-class EagerVisDaDataset(data.Dataset):
-
-	def __init__(self, im_size=visda.shape, mode="eval"):
-		if mode == "train":
-			self.image_fnlist = glob.glob(os.path.join(root_dir, "images", "*.png"))
-			self.label_fnlist = [fn.replace("images", "annotations") for fn in self.image_fnlist]
-		else:
-			self.image_fnlist = glob.glob(os.path.join(root_dir, "eval", "images", "*.png"))
-			self.label_fnlist = [fn.replace("images", "annotations") for fn in self.image_fnlist]
-
-		self.num_classes = visda.num_classes
-		self.img_mean = visda.img_mean
-		self.img_stdev = visda.img_stdev
-
-		self.class_weights = torch.FloatTensor(visda.class_weights)
-
-		self.size = len(self.image_fnlist)
-		self.img_size = im_size
-		self.shape = im_size
-		self.default_size = visda.shape
-
-		pool = mp.Pool(PROCESSORS)
-		self.data = pool.starmap(self.load_img, zip(self.image_fnlist, self.label_fnlist))
-
-	def load_img(self, img_fn, lbl_fn):
-
-		img = cv2.imread(img_fn)
-		lbl = cv2.imread(lbl_fn)
-
-		size = (self.shape[1], self.shape[0])
-		img = cv2.resize(img, size, interpolation=cv2.INTER_AREA)
-		lbl = cv2.resize(lbl, size, interpolation=cv2.INTER_NEAREST)
-
-		lbl = transform_labels(lbl)
-
-		img = img - self.img_mean
-		img /= self.img_stdev
-
-		img = torch.from_numpy(img).permute(2, 0, 1).type(torch.FloatTensor)
-		lbl = torch.from_numpy(lbl).type(torch.LongTensor)
-
-		return (img, lbl)
-
-	def __getitem__(self, index):
-		return self.data[index]
-
-	def __len__(self):
-		return self.size
-
-	def get_original(self, index):
-		img_fn = self.image_fnlist[index]
-		lbl_fn = self.label_fnlist[index]
-
-		img = cv2.imread(img_fn)
-		lbl = cv2.imread(lbl_fn)
-
-		lbl = transform_labels(lbl)
-
-		return (img, lbl)
-
 
 ## Helper Functions: ##
 
--- models/gcn_comb.py
+++ models/gcn_comb.py
@@ -5,6 +5,8 @@ from torchvision import models
 import os
 from math import floor
 import yaml
+import tqdm
+import numpy as np
 
 from util.setup import load_args
 args = load_args(os.getcwd())
@@ -86,6 +88,31 @@ class _PyramidPoolingModule(nn.Module):
 
 		return out
 
+class _LearnedBilinearDeconvModule(nn.Module):
+	def __init__(self, channels):
+		super(_LearnedBilinearDeconvModule, self).__init__()
+		self.deconv = nn.ConvTranspose2d(channels, channels, kernel_size=4, stride=2, padding=1)
+		self.deconv.weight.data = self.make_bilinear_weights(4, channels)
+		self.deconv.bias.data.zero_()
+
+	def forward(self, x):
+		out = self.deconv(x)
+		return out
+
+	def make_bilinear_weights(self, size, num_channels):
+		factor = (size + 1) // 2
+		if size % 2 == 1:
+			center = factor - 1
+		else:
+			center = factor - 0.5
+		og = np.ogrid[:size, :size]
+		filt = (1 - abs(og[0] - center) / factor) * (1 - abs(og[1] - center) / factor)
+		filt = torch.from_numpy(filt)
+		w = torch.zeros(num_channels, num_channels, size, size)
+		for i in range(num_channels):
+			w[i, i] = filt
+		return w
+
 class GCN_COMBINED(nn.Module):
 	def __init__(self, num_classes, input_size, k=7):
 		super(GCN_COMBINED, self).__init__()
@@ -139,13 +166,19 @@ class GCN_COMBINED(nn.Module):
 		self.brm8 = _BoundaryRefineModule(num_classes)
 		self.brm9 = _BoundaryRefineModule(num_classes)
 
-		self.deconv1 = _UpsampleModule(num_classes)
-		self.deconv2 = _UpsampleModule(num_classes)
-		self.deconv3 = _UpsampleModule(num_classes)
-		self.deconv4 = _UpsampleModule(num_classes)
-		self.deconv5 = _UpsampleModule(num_classes)
+		# self.deconv1 = _UpsampleModule(num_classes)
+		# self.deconv2 = _UpsampleModule(num_classes)
+		# self.deconv3 = _UpsampleModule(num_classes)
+		# self.deconv4 = _UpsampleModule(num_classes)
+		# self.deconv5 = _UpsampleModule(num_classes)
+		
+		self.deconv1 = _LearnedBilinearDeconvModule(num_classes)
+		self.deconv2 = _LearnedBilinearDeconvModule(num_classes)
+		self.deconv3 = _LearnedBilinearDeconvModule(num_classes)
+		self.deconv4 = _LearnedBilinearDeconvModule(num_classes)
+		self.deconv5 = _LearnedBilinearDeconvModule(num_classes)
 
-		self.psp = _PyramidPoolingModule(num_classes, 10, input_size)
+		self.psp = _PyramidPoolingModule(num_classes, 10, input_size, levels=(1, 2, 3, 6, 8))
 		self.final = nn.Sequential(
 			nn.Conv2d(num_classes + self.psp.out_channels, num_classes, kernel_size=3, padding=1),
 			nn.BatchNorm2d(num_classes),
@@ -155,7 +188,7 @@ class GCN_COMBINED(nn.Module):
 		initialize_weights(self.gcm1, self.gcm2, self.gcm3, self.gcm4, self.brm1, self.brm2, self.brm3,
 					self.brm4, self.brm5, self.brm6, self.brm7, self.brm8, self.brm9)
 
-		initialize_weights(self.deconv1, self.deconv2, self.deconv3, self.deconv4, self.deconv5)
+		# initialize_weights(self.deconv1, self.deconv2, self.deconv3, self.deconv4, self.deconv5)
 
 		initialize_weights(self.psp, self.final)
 
