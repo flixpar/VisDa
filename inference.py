e39c71ec7329a24a195af3c17289770e7c88e222
Felix Parker <flixpar@gmail.com>
2018-02-20 01:34:36 -0500

    added inference and testing code

---
+++ inference.py
@@ -0,0 +1,117 @@
+import torch
+from torch import nn
+from torch.utils import data
+from torch.autograd import Variable
+import torch.nn.functional as F
+torch.backends.cudnn.benchmark = True
+
+import os
+import yaml
+from tqdm import tqdm
+
+import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
+
+from models.gcn import GCN
+from models.gcn_densenet import GCN_DENSENET
+from models.gcn_deconv import GCN_DECONV
+from models.gcn_psp import GCN_PSP
+from models.gcn_comb import GCN_COMBINED
+from models.gcn_resnext import GCN_RESNEXT
+from models.pspnet import PSPNet
+from models.unet import UNet
+
+from loaders.test import TestDataset
+
+from util.util import *
+from util.setup import *
+
+
+class Evaluator:
+
+	def __init__(self, dataset):
+		self.dataset = dataset
+		self.img_size = self.dataset.img_size
+
+		self.dataloader = data.DataLoader(self.dataset, batch_size=1, shuffle=False, num_workers=6)
+
+	def eval(self, model):
+		model.eval()
+		save_path = os.path.join(os.getcwd(), "pred")
+
+		loader = tqdm(self.dataloader, total=self.n_samples)
+		for i, l in enumerate(loader):
+			l = list(l)
+
+			# unpack values from loader
+			image		= l[0]
+			name		= l[1][0]
+
+			# inference on image
+			pred = self.predict(model, image)
+			pred = self.upsample(pred)
+
+			# augment with flip
+			img_flip = self.flip_tensor(image, 3)
+			pred_flip = self.predict(model, img_flip)
+			pred_flip = self.upsample(pred_flip)
+			pred_flip = np.flip(pred_flip, 2)
+			pred = (pred/pred.mean()) + (pred_flip/pred_flip.mean())
+
+			# compute output prediction
+			pred = np.argmax(pred, axis=0)
+
+			# save output image
+			pred_out = self.recolor(pred)
+			cv2.imwrite(pred_out, name)
+			
+
+	def predict(self, model, img):
+		img = Variable(img.cuda())
+		output = model(img)
+		pred = F.softmax(output, dim=1).cpu()
+		return pred
+
+	def upsample(self, img):
+		size = self.dataset.default_size
+		upsampler = nn.Upsample(size=size, mode='bilinear')
+		out = upsampler(img)
+		out = np.squeeze(out.data.cpu().numpy())
+		return out
+
+	def flip_tensor(self, x, dim):
+		dim = x.dim() + dim if dim < 0 else dim
+		inds = tuple(slice(None, None) if i != dim else x.new(torch.arange(x.size(i)-1, -1, -1).tolist()).long() for i in range(x.dim()))
+		return x[inds]
+
+	def recolor(self, lbl):
+		out = np.zeros((lbl.shape[0], lbl.shape[1], 3))
+		for label in visda.labels:
+			out[lbl==label.trainId] = label.color
+		return out
+
+
+if __name__ == "__main__":
+
+	version = yaml.load(open("eval.yaml",'r'))["version"]
+	args = load_args(os.path.join(os.getcwd(), "saves{}".format(version)), eval_path=os.getcwd())
+
+	trained_epochs = args.eval["epochs"]
+	save_path = os.path.join(args.paths["project_path"], "saves{}".format(args.eval["version"]), "{}-{}.pth".format(args.model, trained_epochs))
+
+	dataset = TestDataset()
+
+	if args.model=="GCN":				model = GCN(cityscapes.num_classes, args.img_size, k=args.K).cuda()
+	elif args.model=="GCN_DENSENET":	model = GCN_DENSENET(cityscapes.num_classes, args.img_size, k=args.K).cuda()
+	elif args.model=="GCN_DECONV":		model = GCN_DECONV(cityscapes.num_classes, args.img_size, k=args.K).cuda()
+	elif args.model=="GCN_PSP":			model = GCN_PSP(cityscapes.num_classes, args.img_size, k=args.K).cuda()
+	elif args.model=="GCN_COMB":		model = GCN_COMBINED(cityscapes.num_classes, args.img_size).cuda()
+	elif args.model=="GCN_RESNEXT":		model = GCN_RESNEXT(dataset.num_classes, k=self.args.K).cuda()
+	elif args.model=="UNet":			model = UNet(cityscapes.num_classes).cuda()
+	elif args.model=="PSPNet":			model = PSPNet(cityscapes.num_classes, args.img_size).cuda()
+	else: raise ValueError("Invalid model arg.")
+	model.load_state_dict(torch.load(save_path))
+
+	evaluator = Evaluator(dataset)
+	evaluator.eval(model)
+
---
+++ loaders/test.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import glob
+import yaml
+import numpy as np
+import torch
+import cv2
+from torch.utils import data
+from skimage.exposure import equalize_adapthist, rescale_intensity
+import skimage
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+
+from util.setup import load_args
+args = load_args(os.getcwd())
+paths = args.paths
+
+data_dir = paths["cityscapes_val_path"]
+sys.path.append(paths["project_path"])
+
+import util.cityscapes_helper as cityscapes
+
+
+class TestDataset(data.Dataset):
+
+	def __init__(self, im_size=cityscapes.shape, n_samples=30, mode="bgr"):
+		self.image_fnlist = sorted(glob.glob(os.path.join(data_dir, "*_img.png")))
+		self.label_fnlist = [fn.replace("img", "lbl") for fn in self.image_fnlist]
+
+		self.num_classes = cityscapes.num_classes
+		self.img_mean = cityscapes.img_mean
+		self.img_stdev = cityscapes.img_stdev
+		self.img_mode = mode
+
+		self.size = len(self.image_fnlist)
+		self.img_size = im_size
+		self.default_size = cityscapes.shape
+
+		self.n_samples = n_samples
+
+	def __getitem__(self, index):
+		img_fn = self.image_fnlist[index]
+		lbl_fn = self.label_fnlist[index]
+
+		name = img_fn.split('/')[-1].split('_')[0]
+
+		src_img = cv2.imread(img_fn)
+		src_lbl = cv2.imread(lbl_fn, 0)
+
+		src_img = self.enhance_contrast(src_img)
+		src_lbl = self.transform_labels(src_lbl)
+
+		size = (self.img_size[1], self.img_size[0])
+		img = cv2.resize(src_img.copy(), size, interpolation=cv2.INTER_AREA)
+		lbl = cv2.resize(src_lbl.copy(), size, interpolation=cv2.INTER_NEAREST)
+
+		if self.img_mode == "rgb":
+			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+			img = img - np.flip(self.img_mean, 0)
+			img /= np.flip(self.img_stdev, 0)
+		else:
+			img = img - self.img_mean
+			img /= self.img_stdev
+
+		img = torch.from_numpy(img).permute(2, 0, 1).type(torch.FloatTensor)
+		lbl = torch.from_numpy(lbl).type(torch.LongTensor)
+
+		return (img, lbl), (src_img, src_lbl), name
+
+	def __len__(self):
+		return self.n_samples
+
+	def transform_labels(self, lbl):
+		out = np.zeros((lbl.shape[0], lbl.shape[1]))
+
+		for i in range(self.num_classes):
+			n = cityscapes.trainId2label[i].id
+			out[lbl == n] = i
+
+		return out
+
+	def get_original(self, index):
+		img_fn = self.image_fnlist[index]
+		lbl_fn = self.label_fnlist[index]
+
+		img = cv2.imread(img_fn)
+		lbl = cv2.imread(lbl_fn, 0)
+
+		lbl = self.transform_labels(lbl)
+
+		return (img, lbl)
+
+	def enhance_contrast(self, img):
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		img = equalize_adapthist(img)
+		img = rescale_intensity(img, out_range='uint8').astype(np.uint8)
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+		return img
